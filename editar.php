e748d42988fb8f6556a022eabef207c94e842585
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 19:31:26 -0300

    Update editar.php

--- editar.php
+++ editar.php
@@ -1,5 +1,17 @@
-$sql = "UPDATE contatos SET
-            nome='$nome',
-            email='$email',
-            telefone='$telefone'
-            WHERE id=$id";
+<?php
+include "conexao.php";
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM contatos WHERE id=$id";
+$result = $conn->query($sql);
+$contato = $result->fetch_assoc();
+?>
+
+<form action="salvar.php" method="POST">
+    <input type="hidden" name="id" value="<?= $contato['id'] ?>">
+    <input type="text" name="nome" value="<?= $contato['nome'] ?>">
+    <input type="email" name="email" value="<?= $contato['email'] ?>">
+    <input type="text" name="telefone" value="<?= $contato['telefone'] ?>">
+    <button type="submit">Salvar</button>
+</form>
