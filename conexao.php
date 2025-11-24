699b1a52f12f242021fd428ae8264e7905b28102
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 18:52:49 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,1 +1,22 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "agenda";
 
+$conn = new mysql("$host, $usuario, $senha, $agenda");
+
+if($conn->conect_error) {
+  die("Falha na conexão: ". $conn->conect_error);
+}
+
+CREATE DATABASE agenda;
+USE agenda;
+
+CREATE TABLE contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
+?>
