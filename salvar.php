fdcb080b5c6b8c9024fa2bff02792f10581098b7
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 19:21:44 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -1,1 +1,16 @@
+<?php
+include "conexao.php";
 
+$id = $_POST['id'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+
+if ($id == "") {
+    $sql = "INSERT INTO contatos (nome, email, telefone) VALUES ('$nome', '$email', '$telefone')";
+} 
+
+$conn->query($sql);
+header("Location: index.php");
+exit;
+?>
