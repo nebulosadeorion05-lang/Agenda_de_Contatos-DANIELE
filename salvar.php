bf2f6c085c32cc279db6515b23b6088e512e71cd
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 19:33:08 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -8,7 +8,13 @@ $telefone = $_POST['telefone'];
 
 if ($id == "") {
     $sql = "INSERT INTO contatos (nome, email, telefone) VALUES ('$nome', '$email', '$telefone')";
-} 
+} else {
+    $sql = "UPDATE contatos SET
+            nome='$nome',
+            email='$email',
+            telefone='$telefone'
+            WHERE id=$id";
+}
 
 $conn->query($sql);
 header("Location: index.php");
