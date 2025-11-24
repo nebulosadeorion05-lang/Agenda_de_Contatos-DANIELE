d4bd1b2baa2e9af9559b37c023b2ef851abc4ce3
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 18:56:06 -0300

    Update style.php

--- style.php
+++ style.php
@@ -1,1 +1,28 @@
-
+body {
+      font-family: Arial, sans-serif;
+      background-color: white;
+      margin: 0.5rem;
+      padding: 0.5rem;
+    }
+    h1, h2 {
+      color: #4B0082;
+    }
+    h1 {
+      text-align: center;
+    }
+    .form-container {
+      width: 80%;
+      margin: auto;
+    }
+    .form label {
+      font-weight: bold;
+    }
+    button {
+      background-color: #4B0082;
+      border: none;
+      padding: 8px 12px;
+      color: white;
+      border-radius: 4px;
+      cursor: pointer;
+      margin-right: 10px;
+    }
