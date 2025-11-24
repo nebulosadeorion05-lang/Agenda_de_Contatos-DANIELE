d63f4e6bc50906392293d742f9648b99e1a2e65b
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 19:03:11 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,21 @@
+<?php
+require_once "conexao.php";
 
+if(isset($_POST["excluir"])) {
+
+    $ID = isset($_POST["ID"]) ? (int)$_POST["ID"] : null;
+
+    $stmt = $conexao->prepare("DELETE FROM agenda WHERE ID = ?");
+    if ($stmt === false) {
+        header("Location: index.php");
+        exit;
+    }
+    $stmt->bind_param("i", $ID);
+
+    $stmt->execute();
+    $stmt->close();
+
+    header("Location: index.php");
+    exit;
+}
+?>
