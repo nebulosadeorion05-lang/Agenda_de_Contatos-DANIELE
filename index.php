814738608326b39be30e5fe09227524359f8c2ea
nebulosadeorion05-lang <nebulosa.de.orion05@gmail.com>
2025-11-24 18:53:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
 <body>
   <h1>Cadastro de Contatos</h1>
   <div class="form-container">
-  <form method="post" action="editar.php">
+  <form method="post" action="salvar.php">
     <label for="ID">ID:</label><br>
     <input type="number" id="ID" name="ID" required><br><br>
     <label for="nome">Nome:</label><br>
     <input type="text" id="nome" name="nome"><br><br>
     <label for="telefone">Número de Telefone:</label><br>
     <input type="number" id="telefone" name="telefone"><br><br>
-    <button type="submit" name="editar.php">Cadastrar</button>
+    <button type="submit" name="salvar.php">Cadastrar</button>
     <button type="reset">Limpar</button>
   </form>
   </div>
